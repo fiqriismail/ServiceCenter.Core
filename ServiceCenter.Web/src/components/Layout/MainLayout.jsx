6c050fade3ed18f30e7e7173eabbfaae21de4150
Fiqri Ismail <fiqri.ismail@gmail.com>
2023-04-01 14:15:43 +0530

    React App added and main navigation added

---
+++ ServiceCenter.Web/src/components/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import NavBar from "./NavBar";
+
+function MainLayout() {
+    return (
+        <NavBar />
+    )
+}
+
+export default MainLayout;
---
+++ ServiceCenter.Web/src/components/Layout/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <section id="header">
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">Service Station</span>
+                </div>
+            </nav>
+        </section>
+    )
+}
+
+export default NavBar;
